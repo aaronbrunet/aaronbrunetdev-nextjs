d8bd5466e1dc8751c8c2d13eeee4923a98d84e55
aaronbrunet <awbrunet@gmail.com>
2021-08-26 21:52:25 -0400

    UPdated content centering

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,16 +23,13 @@ export default function Home() {
           </div>
 
         </div> */}
- <main className='min-h-screen p-12'>
-      
+ <main className='min-h-screen p-12 flex flex-col'>
+      <div className='justify-center self-center align-middle'>
       <h1 className='text-5xl pb-8'>Hi, I'm Aaron 👋</h1>
-      <h3 className='text-2xl'>
-          Welcome to <a className='text-blue-500 border-b-2 border-blue-500' href="https://aaronbrunet.dev">aaronbrunet.dev!</a>
-        </h3>
-
         <p className='text-xl'>
         I'm a developer based in Orlando, Florida, with a special interest in emerging technologies and the changing face of the web.
-        </p>      
+        </p>     
+        </div> 
       </main>
 
       <footer className={styles.footer}>
